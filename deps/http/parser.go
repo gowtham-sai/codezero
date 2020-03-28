a73d412f629e9d7eed18adbe83706031681c92f2
Gowtham Sai <dev@gowtham-sai.com>
2020-03-29 03:29:36 +0530

    Remove err check as yaml marshal will never return err

--- deps/http/parser.go
+++ deps/http/parser.go
@@ -1,8 +1,6 @@
 package http
 
 import (
-	"log"
-
 	"gopkg.in/yaml.v2"
 
 	"codezero/deps"
@@ -10,19 +8,10 @@ import (
 
 func parseDependency(spec deps.Spec) (d Dependencies, err error) {
 	err = yaml.Unmarshal([]byte(spec), &d)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-		return
-	}
 	return
 }
 
-
 func parseScenario(spec deps.Spec) (s Scenario, err error) {
 	err = yaml.Unmarshal([]byte(spec), &s)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-		return
-	}
 	return
 }
