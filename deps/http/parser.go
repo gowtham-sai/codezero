2ae237f480d33749a42cf7b7299d75bb50b5e43f
Gowtham Sai <dev@gowtham-sai.com>
2020-03-28 18:14:56 +0530

    Add parse scenario for http dependency

--- deps/http/parser.go
+++ deps/http/parser.go
@@ -8,8 +8,18 @@ import (
 	"codezero/deps"
 )
 
-func parseSpec(spec deps.Spec) (d Dependencies, err error) {
+func parseDependency(spec deps.Spec) (d Dependencies, err error) {
 	err = yaml.Unmarshal([]byte(spec), &d)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+		return
+	}
+	return
+}
+
+
+func parseScenario(spec deps.Spec) (s Scenario, err error) {
+	err = yaml.Unmarshal([]byte(spec), &s)
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
 		return
--- deps/http/parser_test.go
+++ deps/http/parser_test.go
@@ -11,25 +11,25 @@ import (
 	"codezero/deps"
 )
 
-func TestShouldParseSpec(t *testing.T) {
+func TestParseDependency(t *testing.T) {
 	spec, err := ioutil.ReadFile("../../fixtures/deps/http/service_xyz.yml")
 	require.NoError(t, err, "yamlFile.Get error")
 
-	actualHandler, err := parseSpec(deps.Spec(spec))
-	require.NoError(t, err, "http.parseSpec error")
+	actualHandler, err := parseDependency(deps.Spec(spec))
+	require.NoError(t, err, "http.parseDependency error")
 
 	expectedHandler := Handler{
-		Deps: map[ServiceName]Dependency{
-			"voucher_service": Dependency{
+		Deps: Dependencies{
+			"service_xyz": &Dependency{
 				Sits: Situations{
-					"response_2xx": Situation{
-						Req: Request{
+					"response_2xx": &Situation{
+						Req: &Request{
 							Method:  Get,
 							Path:    "/v1/ping",
 							Query:   Query{"waypoints": "102.6123,-6.1235|102.113,-6.2343"},
 							Headers: Header{"Accept-Encoding": []string{"gzip", "compress"}},
 						},
-						Res: Response{
+						Res: &Response{
 							Body: fmt.Sprintf("%s\n", `{"ping": "pong"}`),
 						},
 					},
@@ -38,4 +38,16 @@ func TestShouldParseSpec(t *testing.T) {
 		},
 	}
 	assert.Equal(t, expectedHandler.Deps, actualHandler)
+}
+
+func TestParseScenario(t *testing.T) {
+	actualScenario, err := parseScenario(`
+service_xyz:
+  response_2xx:
+    port: 8010
+`)
+	require.NoError(t, err)
+
+	expectedScenario := Scenario{"service_xyz": map[SituationName]Spec{"response_2xx": {Port: 8010}}}
+	assert.Equal(t, expectedScenario, actualScenario)
 }
