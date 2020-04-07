7a4cc2ffe6a797a7631cf56777792e0e5381a7b3
Gowtham Sai <dev@gowtham-sai.com>
2020-04-08 01:21:50 +0530

    Provide functionality to register dependency handlers
    ---
    Auto Added
    
    [1mLint:(B[m
    	[32m[✓] deps/dep.go(B[m
    	[32m[✓] deps/dep_test.go(B[m
    
    [1mGoImports:(B[m
    	[32m[✓] deps/dep.go(B[m
    	[32m[✓] deps/dep_test.go(B[m
    
    [1mVet:(B[m
    	[32m[✓] Succeeded(B[m
    
    [1mTest:(B[m
    	[32m[✓] deps(B[m
    	[32m[✓] deps/http(B[m
    
    	[36mDONE 30 tests in 0.207s(B[m
    
    [1mPreChecks:(B[m
    	[32m[✓] Succeeded(B[m

--- deps/dep.go
+++ deps/dep.go
@@ -1,13 +1,28 @@
 package deps
 
+import "errors"
+
 var (
+	// ErrTypeRegistered means that a handler for that particular type is already registered
+	// We do not allow duplicate handlers for same type
+	ErrTypeRegistered = errors.New("deps.type.registered")
+
 	handlers = map[Type]Handler{}
 )
 
 type (
+	// Type indicates the Handler type
+	// a type for http Handler might be http and for tcp might be tcp
+	// specs allows only registered handlers
 	Type string
+
+	// Spec as the name says, it indicates the spec for Handler
+	// For a given scenario, all the specs will be passed to registered handler
 	Spec string
 
+	// Handler defines the interface for specific dependencies
+	// any dependencies must implement Handler interface in order
+	// to be a dependency for a scenario
 	Handler interface {
 		StartSituation(spec Spec) error
 		StopSituation(spec Spec) error
@@ -15,6 +30,13 @@ type (
 	}
 )
 
-func RegisterHandler(t Type, h Handler) error {
-	return nil
+// RegisterHandler provides a way for dependency to register
+// it requires type and handler
+// if type is already registered, it throws error
+func RegisterHandler(t Type, h Handler) (err error) {
+	if _, found := handlers[t]; found {
+		return ErrTypeRegistered
+	}
+	handlers[t] = h
+	return
 }
--- deps/dep_test.go
+++ deps/dep_test.go
@@ -7,10 +7,19 @@ import (
 )
 
 func TestRegisterHandler(t *testing.T) {
-	t.Run("register new type Handler", func(t *testing.T) {
+	t.Run("registering new type Handler", func(t *testing.T) {
 		t.Run("should be able to register", func(t *testing.T) {
 			mockHandler := &MockHandler{}
 			assert.NoError(t, RegisterHandler(Type("dummy"), mockHandler))
+		})
+	})
+
+	t.Run("registering existing type Handler", func(t *testing.T) {
+		handlers = map[Type]Handler{}
+		t.Run("should not be able to register", func(t *testing.T) {
+			mockHandler := &MockHandler{}
+			assert.NoError(t, RegisterHandler(Type("mocked"), mockHandler))
+			assert.EqualError(t, RegisterHandler(Type("mocked"), mockHandler), ErrTypeRegistered.Error())
 		})
 	})
 }
